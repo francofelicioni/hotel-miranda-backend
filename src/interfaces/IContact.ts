97705dca235069b27ac949f066e7b9e2d34418d5
Franco Felicioni <fran.felicioni@gmail.com>
2022-12-28 23:57:25 +0100

    IContact interface created

---
+++ src/interfaces/IContact.ts
@@ -0,0 +1,10 @@
+export interface IContact {
+  id: string;
+  customer: string;
+  email: string;
+  phone: string;
+  date: string;
+  subject: string;
+  comment: string;
+  archived: boolean;
+}
