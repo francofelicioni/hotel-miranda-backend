29c2c7ffd976b7dd17758cd6d28d0b6ec2167eb4
Franco Felicioni <fran.felicioni@gmail.com>
2023-01-05 16:49:42 +0100

    Booking controller updated to MongoDB

--- src/controllers/bookings.ts
+++ src/controllers/bookings.ts
@@ -1,25 +1,79 @@
-import { Request, Response } from "express";
-import bookings from "../../data/guests.json";
+import { NextFunction, Request, Response } from "express";
+import { connection, disconnect } from "src/connection";
+import { IBookings } from "src/interfaces/bookings";
+import { bookingModel } from "src/schemas/bookingSchema";
 
-export const getBookings = (req: Request, res: Response) => {
-  return res.json({ bookings: bookings });
+export const getBookings = async (req: Request, res: Response, next: NextFunction) => {
+  await connection();
+  try {
+    const bookings: IBookings[] = await bookingModel.find();
+    res.json(bookings);
+  } catch (err) {
+    next (err);
+  }
+  await disconnect();
 };
 
-export const getBooking = (req: Request, res: Response) => {
+export const getBooking = async (req: Request, res: Response, next: NextFunction) => {
+  await connection();
   const { id } = req.params;
-  return res.json({ booking: bookings.find((booking) => booking["id"] == id) });
+  try {
+    const booking: IBookings[] = await bookingModel.findById(id);
+    res.json(booking);
+  } catch (err) {
+    next(err)
+  }
+  await disconnect();
 };
 
-export const addBooking = (req: Request, res: Response) => {
-  const { data } = req.body;
-  return res.json({ success: true, message: data });
+export const addBooking = async (req: Request, res: Response, next: NextFunction) => {
+  await connection();
+  const booking = new bookingModel(req.body);
+
+  try {
+    const bookingToPost = await booking.save();
+    res.status(201).json({ bookingToPost });
+  } catch (err) {
+    res.status(400).send({
+      error: "Error, check the booking information.",
+    });
+    next(err);
+  }
+  await disconnect();
 };
 
-export const updateBooking = (req: Request, res: Response) => {
-  const { data } = req.body;
-  return res.json({ success: true, message: data });
+export const updateBooking = async (req: Request, res: Response, next: NextFunction) => {
+  await connection();
+  const { id } = req.params;
+  const booking: IBookings = req.body;
+
+  try {
+    const bookingToUpdate = await bookingModel.findByIdAndUpdate({ _id: id }, booking);
+    res.status(201).json({
+      success: "Booking updated", booking: bookingToUpdate
+    });
+  } catch (err) {
+    res.status(400).json({
+      error: "Error, check the booking information.",
+    });
+    next(err);
+  }
+  await disconnect();
 };
 
-export const deleteBooking = (req: Request, res: Response) => {
-  return res.json({ success: true });
+export const deleteBooking = async (req: Request, res: Response, next: NextFunction) => {
+  await connection();
+  const {id} = req.params;
+
+  try {
+    const bookingToDelete : IBookings [] = await bookingModel.findOneAndDelete({'_id': id});
+    res.status(202).json({
+      success: `Booking ${id} was removed.`
+    })
+  } catch (err) {
+    res.status(400).json({
+      error: 'Error, the booking could not be deleted.'
+    })
+    next(err);
+  }
 };
