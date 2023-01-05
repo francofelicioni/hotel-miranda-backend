bc79dd3c4954ba4ee3d7c76f9a08eaad849a39bb
Franco Felicioni <fran.felicioni@gmail.com>
2023-01-05 16:53:06 +0100

    Connection file modified to work with MongoDB

--- src/connection.ts
+++ src/connection.ts
@@ -1,8 +1,19 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-main().catch(err => console.log(err));
+const MONGODB_URL = "mongodb://127.0.0.1:27017/hotel_miranda_backend";
 
-async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/hotel_miranda_backend');
-  
+mongoose.set("strictQuery", false);
+
+export async function connection(): Promise<void> {
+  try {
+    await mongoose.connect(MONGODB_URL);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function disconnect(): Promise<void> {
+  await mongoose.disconnect();
 }
