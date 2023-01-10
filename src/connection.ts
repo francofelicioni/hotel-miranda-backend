6dc48b050deba650cf0024adcc3fecfda6a2ac3a
Franco Felicioni <fran.felicioni@gmail.com>
2023-01-10 22:45:36 +0100

    MONGODB_URL replaced whit ENV

--- src/connection.ts
+++ src/connection.ts
@@ -2,14 +2,16 @@ import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const MONGODB_URL = "mongodb://127.0.0.1:27017/hotel_miranda_backend";
+const MONGODB_URL = process.env.MONGO_URL
 
 mongoose.set("strictQuery", false);
 
 export async function connection(): Promise<void> {
   try {
     await mongoose.connect(MONGODB_URL);
+    console.log('connected')
   } catch (err) {
+
     console.log(err);
   }
 }
